fbe01959e10d2affa8545ddf1aa83c4bc0c71837
Chris Kraft <chris.kraft@kriggity.com>
2021-01-19 10:42:46 -0500

    corrected import name case

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../db').import('../models/User');
+const User = require('../db').import('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // const fetch = require('node-fetch');
